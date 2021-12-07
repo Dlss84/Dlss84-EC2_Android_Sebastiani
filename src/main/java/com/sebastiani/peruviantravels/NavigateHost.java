06166b54085e34da42ec189ec636eba36376fd1a
Dlss84 <77203208+Dlss84@users.noreply.github.com>
2021-12-07 00:47:58 -0500

    Delete src directory

--- src/main/java/com/sebastiani/peruviantravels/NavigateHost.java
+++
@@ -1,8 +0,0 @@
-package com.sebastiani.peruviantravels;
-
-import androidx.fragment.app.Fragment;
-
-public interface NavigateHost{
-
-    void navigateTo(Fragment fragment, boolean addToBackstack);
-}
