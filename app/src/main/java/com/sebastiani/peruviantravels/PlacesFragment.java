0503557b851a4f36676919eddff26084e39c45b0
Dlss84 <77203208+Dlss84@users.noreply.github.com>
2021-12-07 00:49:12 -0500

    Add files via upload

--- app/src/main/java/com/sebastiani/peruviantravels/PlacesFragment.java
+++ app/src/main/java/com/sebastiani/peruviantravels/PlacesFragment.java
@@ -8,11 +8,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 public class PlacesFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            @NonNull LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.places_fragment, container, false);
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.places_fragment,container,false);
+
+
+        MaterialButton nextButton= view.findViewById(R.id.button_next);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((NavigateHost) getActivity()).navigateTo(new FoodFragment(), false);
+
+            }
+        });
+        return view;
     }
 }
